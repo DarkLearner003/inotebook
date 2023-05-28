85fc9e5930de9e611d1a5484d09728733d05f8cc
DarkLearner003 <darklearner003@gmail.com>
2023-05-28 15:44:14 +0530

    mongoose connected

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://0.0.0.0:27017/"
+
+const connectToMongo= async()=> {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log('Connected');
+    } catch (error) {
+        console.error('Connection error:', error);
+    }
+}
+module.exports = connectToMongo; 
