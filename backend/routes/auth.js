6da207d29f648a514a5a5da0e15bc667877a0ddf
DarkLearner003 <darklearner003@gmail.com>
2023-05-31 11:54:50 +0530

    Backend Completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const fetchuser = require('../middleware/fetchuser');
 const { body, validationResult } = require('express-validator');
 
+const JWT_SECRET = 'Harryisagoodb$oy';
 
 
 router.post('/createuser', [
@@ -14,17 +18,71 @@ router.post('/createuser', [
     if (!error.isEmpty()) {
         return res.status(400).json({ errors: error.array() });
     }
-    
+try{
+    let salt= await bcrypt.genSalt(10);
+    const secPas=await bcrypt.hash(req.body.password,salt);;
     let user= await User.findOne({email:req.body.email})
     if(user){
         res.status(400).json({email:"Email already exist"})
     }
     user= await User.create({
         name:req.body.name,
-        password:req.body.password,
+        password:secPas,
         email:req.body.email
     })
-    res.json({"Hello":1})
+
+    const data={
+        user:{
+            id:user.id
+        }
+    }
+    const authToken=jwt.sign(data,JWT_SECRET);
+    res.json({authToken})
+}catch(err){
+    res.status(500).send("Some error occured");
+}
+})
+
+router.post('/login', [
+    body('email','enter a valid mail').isEmail(),
+    body('password',"password cant be blank").exists()
+], async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return res.status(400).json({ errors: error.array() });
+    }
+    const {email,password}=req.body;
+    try {
+        let user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"Please login with correct credentials"})
+        }
+        const passwordCompare=await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"Please login with correct credentials"})
+        }
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        const authToken=jwt.sign(data,JWT_SECRET);
+        res.json({authToken})
+    }catch(err){
+        res.status(500).send("Some error occured");
+    }
+    
+})
+
+router.post('/getuser',fetchuser,async (req, res) => {
+    try {
+        let userId=req.user.id;
+        let user=await User.findById(userId).select("-password");
+        res.send(user);
+    }catch(err){
+        res.status(500).send("Some error occured");
+    }
+    
 })
 
 module.exports = router
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,78 @@
 const express = require('express');
-const router=express.Router();
+const router = express.Router();
+const Notes = require("../models/Notes");
+const fetchuser = require('../middleware/fetchuser');
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
-    res.json([]);
+
+router.get('/fetchalluser', fetchuser, async (req, res) => {
+    try {
+        const notes = await Notes.find({ user: req.user.id })
+        res.json(notes);
+
+    } catch (err) {
+        res.status(500).send("Some error occured");
+    }
+})
+
+router.post('/addnotes', fetchuser, [
+    body('title', 'enter a valid title').isLength({ min: 3 }),
+    body('description', "description must be atleast 5 character long").isLength({ min: 5 })
+], async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+        const error = validationResult(req);
+        if (!error.isEmpty()) {
+            return res.status(400).json({ errors: error.array() });
+        }
+        const note = new Notes({
+            title, description, tag, user: req.user.id
+        })
+        const savednotes = await note.save()
+        res.json(savednotes);
+    } catch (err) {
+        res.status(500).send("Some error occured");
+    }
+})
+
+router.put('/updatenotes/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    try {
+
+        const newNotes = {};
+        if (title) { newNotes.title = title; }
+        if (description) { newNotes.description = description; }
+        if (tag) { newNotes.tag = tag; }
+
+        let notes = await Notes.findById(req.params.id)
+        if (!notes) { return res.status(404).send("Not found") }
+        if (notes.user.toString() !== req.user.id) {
+            return res.status(401).send("Not allowed")
+        }
+
+        notes = await Notes.findByIdAndUpdate(req.params.id, { $set: newNotes }, { new: true });
+        res.json({ notes });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+
+router.delete('/deletenotes/:id', fetchuser, async (req, res) => {
+    try {
+        let notes = await Notes.findById(req.params.id)
+        if (!notes) { return res.status(404).send("Not found") }
+        if (notes.user.toString() !== req.user.id) {
+            return res.status(401).send("Not allowed")
+        }
+
+        notes = await Notes.findByIdAndDelete(req.params.id);
+        res.json({"success":"Dleted successfully",notes:notes});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
 })
 
 module.exports = router
